f7c4c3be6152ecac7436fd792d6bed7558f02194
alchesnokov <thechesnokov@gmail.com>
2019-11-23 18:18:44 +0300

    getting tickets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,61 @@
 import React from 'react';
 import './App.scss';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      React works!
-    </div>
-  );
+axios.get('https://front-test.beta.aviasales.ru/search')
+    .then(response => {
+        let searchId = response.data.searchId;
+        // console.log(searchId);
+
+        axios.get('https://front-test.beta.aviasales.ru/tickets/?searchId=' + searchId)
+            .then(response => {
+                console.log(response.data);
+            });
+    });
+
+export default function App() {
+    return (
+        <div className="app">
+            <div className="app__container">
+                <div className="tickets">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td colSpan="2">13 400 p</td>
+                            <td>logo</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div>mow-hkt</div>
+                                <div>10:45 - 08:45</div>
+                            </td>
+                            <td>
+                                <div>в пути</div>
+                                <div>21ч 15м</div>
+                            </td>
+                            <td>
+                                <div>2 пересадки</div>
+                                <div>hkg, jnb</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div>mow-hkt</div>
+                                <div>10:45 - 08:45</div>
+                            </td>
+                            <td>
+                                <div>в пути</div>
+                                <div>21ч 15м</div>
+                            </td>
+                            <td>
+                                <div>2 пересадки</div>
+                                <div>hkg, jnb</div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default App;
