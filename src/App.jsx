52fd69deb9356e8dd92850870540b9038b114efe
alchesnokov <thechesnokov@gmail.com>
2019-11-25 04:06:05 +0300

    add data into tickets

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import axios from 'axios';
 
-axios.get('https://front-test.beta.aviasales.ru/search')
-    .then(response => {
-        let searchId = response.data.searchId;
-        // console.log(searchId);
+export default function App() {
 
-        axios.get('https://front-test.beta.aviasales.ru/tickets/?searchId=' + searchId)
+    const [tickets, setTickets] = useState([]);
+
+    useState(() => {
+
+        // axios.get('https://front-test.beta.aviasales.ru/search')
+        //     .then(response => {
+        //         let searchId = response.data.searchId;
+        //         // console.log(searchId);
+        //
+        //         axios.get('https://front-test.beta.aviasales.ru/tickets/?searchId=' + searchId)
+        //             .then(response => setTickets(response.data))
+        //     });
+
+        // fetch('https://api.punkapi.com/v2/beers')
+        //     .then(response => response.json())
+        //     .then(data => setTickets(data))
+
+        axios.get('https://front-test.beta.aviasales.ru/search')
             .then(response => {
-                console.log(response.data);
-            });
+                let searchId = response.data.searchId;
+
+                axios.get('https://front-test.beta.aviasales.ru/tickets/?searchId=' + searchId)
+                    .then((response => {
+                        let tickets = response.data.tickets;
+                        console.log(tickets);
+                        setTickets(tickets);
+                    }))
+            })
     });
 
-export default function App() {
     return (
         <div className="app">
             <div className="app__container">
-                <div className="tickets">
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td colSpan="2">13 400 p</td>
-                            <td>logo</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div>mow-hkt</div>
-                                <div>10:45 - 08:45</div>
-                            </td>
-                            <td>
-                                <div>в пути</div>
-                                <div>21ч 15м</div>
-                            </td>
-                            <td>
-                                <div>2 пересадки</div>
-                                <div>hkg, jnb</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div>mow-hkt</div>
-                                <div>10:45 - 08:45</div>
-                            </td>
-                            <td>
-                                <div>в пути</div>
-                                <div>21ч 15м</div>
-                            </td>
-                            <td>
-                                <div>2 пересадки</div>
-                                <div>hkg, jnb</div>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
+                {
+                    tickets.map(item =>
+                        <div className="tickets">
+                            <table>
+                                <tbody>
+                                <tr>
+                                    <td colSpan="2">{item.price}</td>
+                                    <td>
+                                        <img src={`//pics.avs.io/99/36/${item.carrier}.png`} alt="logo"/>
+                                    </td>
+                                </tr>
+                                {
+                                    item.segments.map(item =>
+                                        <tr>
+                                            <td>
+                                                <div>{item.origin} {item.destination}</div>
+                                                <div>{item.date}</div>
+                                            </td>
+                                            <td>
+                                                <div>в пути</div>
+                                                <div>{item.duration}</div>
+                                            </td>
+                                            <td>
+                                                <div>пересадки</div>
+                                                <div>{`${item.stops[0]} ${item.stops[1]} ${item.stops[2]}`}</div>
+                                            </td>
+                                        </tr>)
+                                }
+                                </tbody>
+                            </table>
+                        </div>)
+                }
             </div>
         </div>
     );
